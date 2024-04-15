604a8fba4cc63daf430c306535626bf6f6dc4075
santisoftpro <santimasoft@gmail.com>
2024-04-15 20:14:06 +0200

    size

--- admin/assets/js/images.js
+++ admin/assets/js/images.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         var files = this.files;
         $(files).each(function(i, file){
             console.log(file)
-            if(check_image_extension(file.name))
+            if(check_image_extension(file.name) && check_image_size(file))
             {
                 alert('true');
             }
@@ -26,5 +26,19 @@ function check_image_extension(fileName)
         $('#errors').append('<div class="alert alert-danger">'+fileName+' has not valid extension.pleaase usee jpg,jpeg and png</div>');
 
         return false
+    }
+}
+function check_image_size(file)
+{
+    if(file.size<=2000000)
+    {
+        return true;
+    }
+    else 
+    {
+        $('#errors').show();
+        $('#errors').append('<div class="alert alert-danger">'+file.name+' has not valid Greater than 2MB </div>');
+
+        return false 
     }
 }
