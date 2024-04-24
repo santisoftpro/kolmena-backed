17b29b8d4e28a6dd8cb1abfcca8fdbafd5249543
santisoftpro <santimasoft@gmail.com>
2024-04-24 03:29:36 +0200

    qwer

---
+++ kolmena/hybrid.php
@@ -0,0 +1,414 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Page Title -->
+    <title>Service</title>
+    <!-- Favicon -->
+    <link rel="icon" href="../kolmena/img/favcon.png">
+    <!-- Bundle -->
+    <link rel="stylesheet" href="../vendor/css/bundle.min.css">
+    <!-- Plugin Css -->
+    <link rel="stylesheet" href="../vendor/css/revolution-settings.min.css">
+    <link rel="stylesheet" href="../vendor/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../vendor/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../vendor/css/swiper.min.css">
+    <link rel="stylesheet" href="../vendor/css/cubeportfolio.min.css">
+    <link rel="stylesheet" href="../vendor/css/LineIcons.min.css">
+    <link rel="stylesheet" href="../vendor/css/jquery-ui.bundle.css">
+    <link rel="stylesheet" href="../vendor/css/select2.min.css">
+    <link rel="stylesheet" href="../vendor/css/slick-theme.css">
+    <link rel="stylesheet" href="../vendor/css/slick.css">
+    <link rel="stylesheet" href="../vendor/css/animate.css">
+    <!-- Style Sheet -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+/* Custom styles for dropup */
+.dropdown-menu.dropup {
+    /* bottom: 100%; */
+    top: auto;
+    position: relative;
+}
+.container a.check
+{
+    color: #fff;
+    border-radius: 5px;
+
+}
+
+</style>
+<body class="position-relative">
+
+<!--Preloader-->
+
+<div class="loader" id="loader-fade">
+    <div class="position-relative">
+        <div class="load">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+</div>
+
+<!--End Preloader-->
+
+
+<!--Header Start-->
+<header class="cursor-light">
+
+    <!--Navigation-->
+    <nav class="navbar navbar-top-default nav-radius navbar-expand-lg">
+        <div class="container">
+            <a href="../index.php" title="Logo" class="logo">
+                <!--Logo Default-->
+                <img src="img/logo.png" alt="logo" class="logo-dark logo-default">
+                <img src="img/logo.png" alt="logo" class="logo-light logo-scrolled">
+                </a>
+                <span class="logo-text bold-and-large">KOLMENA GROUP</span>
+
+            <!--Nav Links-->
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav ml-250">
+                    <a class="nav-link" href="../index.php#home">Home</a>
+                    <a class="nav-link" href="../index.php#about">About</a>
+                    <a class="nav-link" href=../index.php#work>Projects</a>
+                    <a class="nav-link" href="../index.php#news">Blog</a>
+                    <a class="nav-link active" href="news.php">Service</a>
+                    <a class="nav-link" href="../index.php#contactus">Contact</a>
+                    <a class="nav-link" href="../home-page3.php">Product</a>
+                </div>
+
+            </div>
+            <!--Side Menu Button-->
+            <a href="javascript:void(0)" class="parallax-btn sidemenu_btn" id="sidemenu_toggle">
+                <div class="animated-wrap sidemenu_btn_inner">
+                    <div class="animated-element">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </nav>
+
+    <!--Side Nav-->
+    <div class="side-menu hidden">
+        <div class="inner-wrapper">
+            <span class="btn-close link" id="btn_sideNavClose"></span>
+            <nav class="side-nav w-100">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php#about">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php#work">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.phpnews.html">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="news.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php#contactus">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="side-footer text-white w-100">
+                <ul class="social-icons-simple">
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i class="fab fa-facebook-f"></i> </a> </li>
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i class="fab fa-instagram"></i> </a> </li>
+                    <li class="animated-wrap"><a class="animated-element" href="javascript:void(0)"><i class="fab fa-twitter"></i> </a> </li>
+                </ul>
+                <p class="text-white">&copy; 2024 KOLMENA GROUP. Made With Love by INETWORKA LTD</p>
+            </div>
+        </div>
+    </div>
+    <a id="close_side_menu" href="javascript:void(0);"></a>
+    <!-- End side menu -->
+</header>
+<!--Header end-->
+
+<br>
+<br>
+   <!-- design -->
+   <section class = "design" id = "design">
+    <div class = "container" id="hybrid">
+       
+      <div class = "title">
+        <h2>Flat Plate Solar Water Heater</h2>
+        <p>standalone systems and hybrid systems</p>
+        
+      </div>
+
+      <div class = "design-content">
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/IMG-20231211-WA0020.jpg" alt = "">
+            <span></span>
+            <span>SOLAR HEATING SYSTEM</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">this is a cost-effective way to generate hot water for your home or hotels.</a>
+          </div>
+        </div>
+        <!-- end of item -->
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/IMG-20231020-WA0000.jpg">
+            <span></span>
+            <span>SOLAR OUTDOOR LIGHTING</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">smart solar-powered light emitting diode (LED) outdoor lighting system</a>
+          </div>
+        </div>
+        <!-- end of item -->
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/Pasted image.png" alt = "">
+            <span></span>
+            <span>SOLAR POWER GENERATION</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">Is a system of generating electricity by use of solar energies ,electricity and heat.</a>
+          </div>
+        </div>
+        <!-- end of item -->
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/Pasted image 1.png" alt = "">
+            <span></span>
+            <span>Water pumping system</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">this is a method of pumping water so that it can be distributed from one place to another.</a>
+          </div>
+        </div>
+        <!-- end of item -->
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/Pasted image 2.png" alt = "">
+            <span></span>
+            <span>Solar mini grid</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">are independent decentralised electricity networks that can function separately from a national grid.</a>
+          </div>
+        </div>
+        <!-- end of item -->
+        <!-- item -->
+        <div class = "design-item">
+          <div class = "design-img">
+            <img src = "new-image/Pasted image 3.png" alt = "">
+            <span></span>
+            <span>COMMERCIAL USE</span>
+          </div>
+          <div class = "design-title">
+            <a href = "#">The Sun’s rays fall on the collector panel (a component of solar water heating system),</a>
+          </div>
+        </div>
+        <!-- end of item -->
+      </div>
+    </div>
+  </section>
+  <!-- end of design -->
+
+
+    <!-- blog -->
+    <section class = "blog" id = "blog">
+        <div class = "container">
+          <div class = "title">
+            <h2>Solar Heating System</h2>
+            <p>Flat Plate Solar Water Heater</p>
+          </div>
+          <div class = "blog-content">
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 4.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>Solar Heating System</h2>
+                <p>Flat plate collectors can heat the fluid inside using either direct or indirect sunlight from a wide range of different angles.</p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 5.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>Flat Plate Collector</h2>
+                <p>A direct solar water heating system, also known as an active open-loop system, uses a pump to circulate the water around the system.</p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 6.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>Flat Plate Collector on Roof</h2>
+                <p>These copper pipes are bonded, soldered or brazed directly to the absorber plate to ensure maximum surface contact and heat transfer.</p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 7.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>Hybrid Water Heating System</h2>
+                <p>In many cases, technology is not yet sufficiently developed to feed a hot water installation entirely with sustainable energy. </p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 8.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>Solar Thermal System</h2>
+                <p>The heat exchanger transfers the heat from the collector’s antifreeze solution to the water located in the water storage tank.</p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+            <!-- item -->
+            <div class = "blog-item">
+              <div class = "blog-img">
+                <img src = "new-image/Pasted image 9.png" alt = "">
+                <!--<span><i class = "far fa-heart"></i></span>-->
+              </div>
+              <div class = "blog-text">
+                <!-- <span>20 January, 2020</span> -->
+                <h2>solar water heater</h2>
+                <p>SGE is a condensing HE solar water heater with integrated heat exchanger for solar energy. </p>
+                <!-- <a href = "#">Read More</a> -->
+              </div>
+            </div>
+            <!-- end of item -->
+             
+          </div>
+        </div>
+      </section>
+      <!-- end of blog -->
+
+
+
+<!--Form End-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WhatsApp Button</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <a href="https://wa.me/250785843115" class="whatsapp-button" target="_blank">
+        <img src="/kolmena/img/whatsapp-logo.png" alt="WhatsApp Logo" class="whatsapp-logo">
+        <span class="whatsapp-text">Whatsapp us</span>
+    </a>
+</body>
+</html>
+<!--Footer Start-->
+<section class="text-center bg-blue m-0" id="heating">
+    <h2 class="d-none">hidden</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-social">
+                    <ul class="list-unstyled text-white">
+                         <li><a class="wow fadeInUp facebook-bg-hvr" href="https://www.facebook.com/p/Kolmena-Group-100086357093029/"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
+                        <li><a class="wow fadeInDown twitter-bg-hvr" href="https://twitter.com/kolmenaltd"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a class="wow fadeInDown linkedin-bg-hvr" href="https://www.linkedin.com/company/kolmena-group-ltd"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a></li>
+                        <li><a class="wow fadeInUp instagram-bg-hvr" href="https://www.instagram.com/kolmenagroup/"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+                <p class="company-about fadeIn text-capitalize">&copy; 2024 kolmena Group ltd. Made With Love By <a href="javascript:void(0);">Inetworka ltd</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Footer End-->
+
+
+
+<!--G0 Top-->
+<div class="go-top"><i class="fas fa-chevron-up"></i><i class="fas fa-chevron-up"></i></div>
+<!--End Go Top-->
+
+
+<!-- JavaScript -->
+<script src="../vendor/js/bundle.min.js"></script>
+<!-- Plugin Js -->
+<script src="../vendor/js/jquery.fancybox.min.js"></script>
+<script src="../vendor/js/owl.carousel.min.js"></script>
+<script src="../vendor/js/swiper.min.js"></script>
+<script src="../vendor/js/jquery.cubeportfolio.min.js"></script>
+<script src="../vendor/js/jquery.appear.js"></script>
+<script src="../vendor/js/wow.min.js"></script>
+<script src="../vendor/js/flip.js"></script>
+<script src="../vendor/js/jquery-ui.bundle.js"></script>
+<script src="../vendor/js/select2.min.js"></script>
+<script src="../vendor/js/jquery.hoverdir.js"></script>
+<script src="../vendor/js/hover-item.js"></script>
+<script src="../vendor/js/slick.min.js"></script>
+<script src="../vendor/js/parallaxie.min.js"></script>
+<!-- REVOLUTION JS FILES -->
+<script src="../vendor/js/jquery.themepunch.tools.min.js"></script>
+<script src="../vendor/js/jquery.themepunch.revolution.min.js"></script>
+<!-- SLIDER REVOLUTION EXTENSIONS -->
+<script src="../vendor/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="../vendor/js/extensions/revolution.extension.video.min.js"></script>
+<!-- custom script -->
+<script src="http://maps.google.com/maps/api/js?key=AIzaSyCo_pcAdFNbTDCAvMwAD19oRTuEmb9M50c"></script>
+<script src="js/maps.min.js"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
+
